916a3bbe8dc9eccc6f5f85d49c7779ac4c19fac3
FruityFly <Katmendos@icloud.com>
2025-11-11 21:46:37 -0800

    Scenes 4+5, enemies
    Creation of scenes 4 and 5, as well as slime/skeleton/ghost scripts and prefabs and beginning of final boss

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 5.0f;
+    public float range = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, target.position) < range)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
